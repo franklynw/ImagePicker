5a1b3edce7b79b28d4f32cd45239f6627f11fe43
Franklyn Weber <franklyn@somacreate.com>
2022-08-25 11:26:21 +0100

    Added code to ask for photos authorisation before photos are picked; informs consumers of photos authorisation

--- Sources/ImagePicker/PHImagePicker.swift
+++ Sources/ImagePicker/PHImagePicker.swift
@@ -20,7 +20,9 @@ public struct PHImagePicker<T>: UIViewControllerRepresentable {
     @Binding private var selectedImages: PHImagePickerResult?
     @Binding private var active: T?
     
+    private var selectionLimit = 5
     private var targetSize: CGSize = UIScreen.main.bounds.size
+    private var authorizationResponse: ((PHAuthorizationStatus) -> ())?
     
     
     public init(_ active: Binding<T?>, selectedImages: Binding<PHImagePickerResult?>) {
@@ -42,18 +44,39 @@ public struct PHImagePicker<T>: UIViewControllerRepresentable {
     
     public class Coordinator: PHPickerViewControllerDelegate {
         
-        let imagePickerController: PHPickerViewController = {
-            var configuration = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
-            configuration.filter = .images
-            configuration.selectionLimit = 5
-            return PHPickerViewController(configuration: configuration)
-        }()
+        let imagePickerController: PHPickerViewController
         
         private let parent: PHImagePicker
         
 
         init(parent: PHImagePicker) {
+            
+            let authorizationStatus = PHPhotoLibrary.authorizationStatus(for: .readWrite)
+            if authorizationStatus == .notDetermined {
+                PHPhotoLibrary.requestAuthorization(for: .readWrite) { status in
+                    
+                    parent.authorizationResponse?(status)
+                    
+                    if !(status == .authorized || status == .limited) {
+                        parent.active = nil
+                    }
+                }
+            } else {
+                parent.authorizationResponse?(authorizationStatus)
+                
+                if !(authorizationStatus == .authorized || authorizationStatus == .limited) {
+                    parent.active = nil
+                }
+            }
+            
             self.parent = parent
+            
+            var configuration = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
+            configuration.filter = .images
+            configuration.selectionLimit = parent.selectionLimit
+            
+            imagePickerController = PHPickerViewController(configuration: configuration)
+            
             imagePickerController.delegate = self
         }
         
@@ -80,11 +103,11 @@ public struct PHImagePicker<T>: UIViewControllerRepresentable {
                 
                 group.enter()
                 
-                PHImageManager.default().requestImage(for: asset, targetSize: parent.targetSize, contentMode: .aspectFit, options: nil) { image, _ in
-                    if let image = image {
+                PHImageManager.default().requestImage(for: asset, targetSize: parent.targetSize, contentMode: .aspectFit, options: nil) { image, info in
+                    if info?[PHImageResultIsDegradedKey] as? Int == 0, let image = image {
                         images.append(image)
+                        group.leave()
                     }
-                    group.leave()
                 }
             }
             
@@ -99,9 +122,36 @@ public struct PHImagePicker<T>: UIViewControllerRepresentable {
 
 extension PHImagePicker {
     
-    func targetImageSize(_ targetSize: CGSize) -> Self {
+    public func selectionLimit(_ selectionLimit: Int) -> Self {
+        var copy = self
+        copy.selectionLimit = selectionLimit
+        return copy
+    }
+    
+    public func targetImageSize(_ targetSize: CGSize) -> Self {
         var copy = self
         copy.targetSize = targetSize
         return copy
+    }
+    
+    public func authorizationResponse(_ authorizationResponse: @escaping (PHAuthorizationStatus) -> ()) -> Self {
+        var copy = self
+        copy.authorizationResponse = authorizationResponse
+        return copy
+    }
+}
+
+
+extension PHAuthorizationStatus {
+    
+    public var canAccessPhotos: Bool {
+        switch self {
+        case .notDetermined, .restricted, .denied:
+            return false
+        case .authorized, .limited:
+            return true
+        @unknown default:
+            return false
+        }
     }
 }
